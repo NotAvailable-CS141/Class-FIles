0c9465b07364dfc1d7b555722595e3fa956b3984
TheDrGoldeneye <33264484+TheDrGoldeneye@users.noreply.github.com>
2017-10-31 14:39:01 -0700

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -4,8 +4,20 @@
 package edu.cpp.cs.cs141.sg;
 
 /**
- * @author thedr_000
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
  *
+ * Final Project
+ *
+ * This project is a text-based game where you play as a spy
+ * who must find the briefcase that is hidden in one of nine rooms
+ * inside of a building. There 6 ninjas that are roaming the building
+ * and will kill you if they are close enough to you. There are multiple power
+ * ups in the building such as one that will give you additional ammo if you used
+ * up all your ammo, gain invisibility, and one that will show you where the briefcase is.
+ *
+ * Team Not Available
+ *  Ryan Guidry, Ethan Balderas, Fadhar Castillo, Zhihang Yao, Daniel Gruhn
  */
 public class GameEngine {
 	
