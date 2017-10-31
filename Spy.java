a54d85a2969a1d2a0d45801de0a72e93d6c92253
ethanb42 <33261459+ethanb42@users.noreply.github.com>
2017-10-31 14:31:00 -0700

    Add files via upload

---
+++ Spy.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.finalProject;
+
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Final Project
+ *
+ * <description-of-assignment>
+ *
+ * Team Not Available
+ *  Ryan Guidry, Ethan Balderas, Fadhar Cartillo, Zhihang Yao, Daniel Gruhn
+ */
+
+public class Spy {
+	//fields 
+	private int health=3;
+	private boolean hasBullet=true;
+	private int invincibility=0;
+	
+	
+	public void shoot() {
+		
+	}
+	
+	public void look() {
+		
+	}
+	
+	public void move() {
+		if(invincibility>0) {
+			invincibility--;
+		}
+		
+		
+	}
+	
+	
+
+}
