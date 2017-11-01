f8a98d14e25b52846317def791be22c4ec89f14e
tempest124 <thirdaco@gmail.com>
2017-11-01 08:05:50 -0700

    Update and rename Room to Room.java

--- Room.java
+++ Room.java
@@ -1,5 +1,22 @@
-package qwertyuiop;
+package com.cs.cs141.project;
 
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Final Project
+ *
+ * This project is a text-based game where you play as a spy
+ * who must find the briefcase that is hidden in one of nine rooms
+ * inside of a building. There 6 ninjas that are roaming the building
+ * and will kill you if they are close enough to you. There are multiple power
+ * ups in the building such as one that will give you additional ammo if you used
+ * up all your ammo, gain invincibility, and one that will show you where the briefcase is.
+ *
+ * Team Not Available
+ *  Ryan Guidry, Ethan Balderas, Fadhar Castillo, Zhihang Yao, Daniel Gruhn
+ */
+ 
 public class Room {
 
 	private boolean visible;
