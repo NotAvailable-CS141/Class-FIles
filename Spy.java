6c9cca2dd0f8ae7f7cd0952d0847a24005c67b7f
ethanb42 <33261459+ethanb42@users.noreply.github.com>
2017-11-01 08:37:04 -0700

    Add files via upload

--- Spy.java
+++ Spy.java
@@ -17,19 +17,42 @@ package edu.cpp.cs.cs141.finalProject;
 
 public class Spy {
 	//fields 
-	private int health=3;
+	/**
+	 * The lives of the spy.
+	 * Initial value of 3.
+	 * 
+	 */
+	private int lives=3;
+	
+	/**
+	 * Spy starts with a single bullet.
+	 * 
+	 */
 	private boolean hasBullet=true;
+	
+	/**
+	 * The turns of invincibility left.
+	 */
 	private int invincibility=0;
 	
 	
+	/**
+	 * Allows the spy to shoot an enemy
+	 */
 	public void shoot() {
 		
 	}
 	
+	/**
+	 * Lets the spy see 2 squares in every direction
+	 */
 	public void look() {
 		
 	}
 	
+	/**
+	 * The spy moves in the desired dirrection.
+	 */
 	public void move() {
 		if(invincibility>0) {
 			invincibility--;
