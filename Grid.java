0021b689788c6ab6e33798b21332767dcb3878d5
tempest124 <thirdaco@gmail.com>
2017-11-06 08:14:52 -0800

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -53,4 +53,16 @@ private boolean fogOfWar;
 	{
 		
 	}
+	
+	public String visual()
+	{
+		String board = "";
+		for(int i =0; i<9; i++)
+		{
+			for(int j =0; j<9; j++)
+		{
+			board += grid[i][j];
+		}	
+			board+= "\n";
+		}
 }
