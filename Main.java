2a3ff3ddfd7db5b2e114cca081bfd2915a06141c
MrGuidry <33229556+MrGuidry@users.noreply.github.com>
2017-11-06 14:19:19 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
-/**
- * 
- */
 package edu.cpp.cs.cs141.FinalProject;
+
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Final Project
+ *
+ * This project is a text-based game where you play as a spy
+ * who must find the briefcase that is hidden in one of nine rooms
+ * inside of a building. There 6 ninjas that are roaming the building
+ * and will kill you if they are close enough to you. There are multiple power
+ * ups in the building such as one that will give you additional ammo if you used
+ * up all your ammo, gain invincibility, and one that will show you where the briefcase is.
+ *
+ * Team Not Available
+ *  Ryan Guidry, Ethan Balderas, Fadhar Castillo, Zhihang Yao, Daniel Gruhn
+ */
 
 /**
  * This class depicts the Main methods which is used to run the entire program;
@@ -13,7 +27,7 @@ public class Main {
 	/**
 	 * 
 	 * The main method instantiates a {@link UserInterface} and 
-	 * runs UserInterface's method {@see edu.cpp.cs.cs141.Assignment2.UserInterface#start()}.
+	 * runs UserInterface's method {@see edu.cpp.cs.cs141.FinalProject.UserInterface#start()}.
 	 * 
 	 * @param args is a string args needed to initiate the main method
 	 */
