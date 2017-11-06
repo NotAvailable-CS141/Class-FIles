746518b9f5152d7225d39e6947d617ee2465d175
MrGuidry <33229556+MrGuidry@users.noreply.github.com>
2017-11-06 14:01:13 -0800

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,42 @@
+package edu.cpp.cs.cs141.FinalProject;
+
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Final Project
+ *
+ * This project is a text-based game where you play as a spy
+ * who must find the briefcase that is hidden in one of nine rooms
+ * inside of a building. There 6 ninjas that are roaming the building
+ * and will kill you if they are close enough to you. There are multiple power
+ * ups in the building such as one that will give you additional ammo if you used
+ * up all your ammo, gain invincibility, and one that will show you where the briefcase is.
+ *
+ * Team Not Available
+ *  Ryan Guidry, Ethan Balderas, Fadhar Castillo, Zhihang Yao, Daniel Gruhn
+ */
+
+public class Location {
+
+	private int row;
+	private int column; 
+	
+	public Location(int x, int y) {
+		row = x;
+		column = y;
+	}
+	
+	public void setLocation(int x, int y) {
+		row = x;
+		column = y;
+	}
+	
+	public int[] getLocation() {
+		int[] array = new int[2];
+		array[0] = row;
+		array[1] = column;
+		return array;
+	}
+	
+}
