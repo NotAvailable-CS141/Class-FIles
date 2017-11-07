c263b4313db5784ac2e8329478593730a2057d5f
TheDrGoldeneye <33264484+TheDrGoldeneye@users.noreply.github.com>
2017-11-07 14:37:54 -0800

    Add files via upload

--- UserInterface.java
+++ UserInterface.java
@@ -15,30 +15,66 @@
  *  Ryan Guidry, Ethan Balderas, Fadhar Castillo, Zhihang Yao, Daniel Gruhn
  */
 
-package edu.cpp.cs.cs141.FinalProject;
+package edu.cpp.cs.cs141.project;
 
 import java.util.Scanner;
 
 public class UserInterface {
+	Scanner sc;
+	UserInterface(){
+		sc = new Scanner(System.in);
+	}
+	public int getPlayerChoice() {
+		
+		int playerChoice = sc.nextInt();
+		return playerChoice;
+	}
 	
-	private GameEngine engine = new GameEngine();
-	private Scanner scan = new Scanner(System.in);
-	
-	
-	public void displayMainMenu() {
-		//Displays game menu at the start of the game
+	/**
+	 * The displayMainMenu() method Displays the starting options for the player. These options are 
+	 * 1. Start New Game, 2. Load Game Save, 3. Help. Then prompts the user to choose an option, then proceeds
+	 * to evaluate the user response. Depending on the response, it will return an integer in the range of 0 and 2.
+	 * A return of 0 means that the user wants to exit the game; Return of 1 means that the user wants to Start New Game;
+	 * Return of 2 means that the user wants to load a game save.
+	 * This integer is used in the Main class where the game is started from.
+	 * @param engine
+	 * @return
+	 */
+	public int displayMainMenu() {
+		
+		int playerChoice = 0;
+		
+		while(playerChoice == 0) {
+			System.out.println("1. Start New Game.");
+			System.out.println("2. Load Game Save");
+			System.out.println("3. Help.");
+			System.out.println("Press any other key to Exit the game.");
+			
+			playerChoice = getPlayerChoice();
+			switch (playerChoice) {
+			case 1: 
+				return playerChoice;
+			case 2:
+				return playerChoice;
+			case 3:
+				displayHelp();
+			default:
+				System.out.println("Exiting Game!");
+				System.exit(0);
+			}
+		}
+		return 0;
 	}
 	public void displayHelp() {
 		//Displays game rules
+		System.out.println("In this game, you play as a Spy who is on a mission to retrieve important documents");
+		System.out.println("from a building that is pitch black. There are also ninjas patrolling the building.");
+		System.out.println("The building has 9 rooms, which only one has the precious briefcase containing the documents.");
+		System.out.println("");
 	}
-	public void displayGrid() {
+	public void displayGrid(String grid) {
 		//Displays game grid with all assets
-		
-		System.out.println(engine.getGrid().visual());
-		System.out.println("Word");
-		
-		
-		
+		System.out.print(grid);
 	}
 	public void displayStats() {
 		//Displays the statistics of the game such as lives left
@@ -49,17 +85,15 @@ public class UserInterface {
 	public void displayGameOver() {
 		//Displays game over message if player dies 3 times
 	}
+	
+	public void displayUnexpectedError() {
+		System.out.println("Unexpected Error Ocurred! Exiting Game.");
+	}
 	public void saveGame() {
 		//saves the game to file
 	}
 	public void loadGameSave() {
 		//loads game save from file
-	}
-	public void start() {
-		// TODO Auto-generated method stub
-		displayGrid();
-		
-		
 	}
 
 }
